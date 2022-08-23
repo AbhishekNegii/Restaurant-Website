ac649385cda26f0efbc43cd1d5cd601a3ef12952
Abhisheknegii <abhibaba007@gmail.com>
2022-08-23 17:29:36 +0530

    First Step (Header Part Started )

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Header from "./Components/Cart/Header";
+
+function App() {
+  return (
+    <Fragment>
+       <Header />
+    </Fragment>
+  );
+}
+
+export default App;
