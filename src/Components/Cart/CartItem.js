5824577e67c5acd7d9d73898c853bfac0c6fa484
Abhisheknegii <abhibaba007@gmail.com>
2022-08-26 02:23:53 +0530

    Task 8 (Showing and Enabling + & -)

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,6 +1,19 @@
+import React, {useState} from 'react';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
+    const[quantity, setQuantity]=useState([])
+
+    const cartItemRemoveHandler=()=>{
+        setQuantity(prevCount=>Number(prevCount-1))
+    }
+
+
+    const cartItemAddHandler=()=>{
+        setQuantity(prevCount=>Number(prevCount+1))
+    }
+    
+
   const price = `$${props.price}`;
 //   console.log(price)
 
@@ -11,13 +24,14 @@ const CartItem = (props) => {
         {props.food}
         <div className={classes.summary}>
           <span className={classes.price}>{price}</span>
+          <span className={classes.price}>{quantity}</span>
           <span className={classes.amount}>x {props.amount}</span>
            </div>
       </div>
-      {/* <div className={classes.actions}>
-        <button onClick={props.onRemove}>−</button>
-        <button onClick={props.onAdd}>+</button>
-      </div> */}
+      <div className={classes.actions}>
+        <button onClick={cartItemRemoveHandler}>−</button>
+        <button onClick={cartItemAddHandler}>+</button>
+      </div>
     </li>
   );
 };
